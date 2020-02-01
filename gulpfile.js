3979eff99f0e153a301a1ea75ee23ebf5bd1ea6c
VovaMorghun <vov4ikm1@gmail.com>
2020-02-01 19:35:17 +0200

    add JS

--- gulpfile.js
+++ gulpfile.js
@@ -2,56 +2,57 @@
 
 
 
-   var gulp = require('gulp');
-   var bs = require('browser-sync').create();
-   var gp = require('gulp-load-plugins')();
-    // var autoprefixer = require('gulp-autoprefixer')
+var gulp = require('gulp');
+var bs = require('browser-sync').create();
+var gp = require('gulp-load-plugins')();
+// var autoprefixer = require('gulp-autoprefixer')
 
-   gulp.task('serve', function() {
-      bs.init({
+gulp.task('serve', function() {
+    bs.init({
         server: {
             baseDir: "src"
         }
     });
-    
+
 });
 
-gulp.task('watch' , function(){
-    
+gulp.task('watch', function() {
+
     gulp.watch('src/sass/*.sass', gulp.series('sass')),
-    gulp.watch("src/*.html").on('change', bs.reload)
+        gulp.watch("src/*.html ").on('change', bs.reload),
+        gulp.watch("src/*.js").on('change', bs.reload)
 })
 
 
-gulp.task('sass', function(){
+gulp.task('sass', function() {
     return gulp.src('src/sass/*.sass')
-    .pipe(gp.sass({        
-        'include css' : true
-    }))
-    .pipe(gp.autoprefixer({
-        overrideBrowserslist: ['last 10 versions']
-    }))
-    .on("error", gp.notify.onError({
-        title: "stile"
-    }))
-    .pipe(gp.concatCss('style.css'))    
-    .pipe(gulp.dest('src/css/'))
-    .pipe(bs.reload({
-        stream : true,
-    }));
+        .pipe(gp.sass({
+            'include css': true
+        }))
+        .pipe(gp.autoprefixer({
+            overrideBrowserslist: ['last 10 versions']
+        }))
+        .on("error", gp.notify.onError({
+            title: "stile"
+        }))
+        .pipe(gp.concatCss('style.css'))
+        .pipe(gulp.dest('src/css/'))
+        .pipe(bs.reload({
+            stream: true,
+        }));
 });
 
 
 
 gulp.task('default', gulp.series(
     gulp.parallel('sass'),
-    gulp.parallel('watch' ,'serve')
-    
+    gulp.parallel('watch', 'serve')
+
 ));
 
 
 gulp.task('build', gulp.series(
     gulp.parallel('sass'),
-    gulp.parallel('watch' ,'serve')
-    
+    gulp.parallel('watch', 'serve')
+
 ));
--- src/index.js
+++ src/index.js
@@ -1,18 +1,72 @@
-$(document).ready(function() {
+$(function() {
 
-    var $item = $('.accordione-item');
-    var $header = $('.accordione-header');
+    // fixed header
 
-    $header.on('click', function() {
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
 
-        if (!($(this).parent().hasClass('active'))) {
-            $item.removeClass('active');
-            $(this).parent().addClass('active');
-            return
+    checkSkroll(scrollOffset);
+
+    $(window).on('scroll', function() {
+
+        scrollOffset = $(this).scrollTop();
+        checkSkroll(scrollOffset);
+
+    });
+
+    function checkSkroll(scrollOffset) {
+
+        scrollOffset >= introH ? header.addClass("fixed") :
+            header.removeClass("fixed");
+    };
+
+    //toggle
+    var toggle = $('#toggle');
+
+    toggle.on('click', function() {
+        toggle.toggleClass('active');
+        $('#nav').toggleClass('active');
+    });
+
+
+
+    // accordion
+
+
+
+    $("[data-collapse]").on('click', function(event) {
+
+        event.preventDefault();
+        var $this = $(this);
+        blockId = $this.data('collapse');
+        $this.toggleClass('active')
+        $(blockId).slideToggle(500)
+    });
+
+
+
+
+    // scroll
+
+
+    $('[data-scroll]').on('click', function(event) {
+
+        event.preventDefault();
+
+        var blockId = $(this).data('scroll'),
+            offsetBlock = ($(blockId).offset().top)
+
+        if (($(window).width()) < 576) {
+            offsetBlock -= 300
         }
-        $(this).parent().removeClass('active');
+        $('#nav a').removeClass('active')
+        $(this).addClass('active')
 
 
+        $('html , body').animate({
+            scrollTop: offsetBlock
+        }, 2000)
 
     });
 
@@ -20,6 +74,23 @@ $(document).ready(function() {
 
 });
 
+// // slider
+// var doc = document;
+// var index = 1;
+
+
+
+// var Slider = function() {
+//     this.box = doc.querySelector('.slider-wrapper');
+//     this.slideBox = doc.querySelector('.slider-slides');
+//     this.slide = doc.querySelectorAll('.slider-item');
+//     this.btns = doc.querySelectorAll('.sliderbutton');
+//     this.size = this.box.clientWidth;
+
+//     this.position();
+//     this.carusel();
+
+// };
 
 
 
@@ -27,74 +98,168 @@ $(document).ready(function() {
 
 
 
-(function() {
 
-    var doc = document;
-    var index = 1;
+// Slider.prototype.position = function() {
+//     var size = this.size;
+//     this.slideBox.style.transform = `translateX(${-index * size}px)`;
+// };
+
+// Slider.prototype.carusel = function() {
+//     var i,
+//         max = this.btns.length;
+//     that = this;
+
+//     for (var i = 0; i < max; i += 1) {
+//         that.btns[i].addEventListener('click', Slider[that.btns[i].id].bind(null, that));
+//     }
+// }
+
+// Slider.prev = function(box) {
+//     box.slideBox.style.transition = `transform .7s ease-in-out`;
+//     var size = box.size;
+//     index <= 0 ? false : index--
+//         box.slideBox.style.transform = `translateX(${-index * size}px)`;
+//     box.jump()
+// };
+
+// Slider.next = function(box) {
+//     box.slideBox.style.transition = `transform .7s ease-in-out`;
+//     var max = box.slide.length;
+//     var size = box.size;
+//     index >= max - 1 ? false : index++;
+//     box.slideBox.style.transform = `translateX(${-index * size}px)`;
+//     box.jump();
+// };
+
+
+// Slider.prototype.jump = function() {
+//     var that = this;
+//     var size = this.size;
+//     this.slideBox.addEventListener('transitionend', function() {
+//         that.slide[index].id === `firstClone` ? index = 1 : index;
+//         that.slide[index].id === `lastClone` ? index = that.slide.length - 2 : index;
+
+//         that.slideBox.style.transition = `none`;
+//         that.slideBox.style.transform = `translateX(${-index * size}px)`;
+//     });
 
 
 
-    var Slider = function() {
-        this.box = doc.querySelector('.slider-wrapper');
-        this.slideBox = doc.querySelector('.slider-slides');
-        this.slide = doc.querySelectorAll('.slider-item');
-        this.btns = doc.querySelectorAll('.sliderbutton');
-        this.size = this.box.clientWidth;
 
-        this.position();
-        this.carusel();
+// }
 
-    };
+// new Slider();
 
 
 
-    Slider.prototype.position = function() {
-        var size = this.size;
-        this.slideBox.style.transform = `translateX(${-index * size}px)`;
-    };
 
-    Slider.prototype.carusel = function() {
-        var i,
-            max = this.btns.length;
-        that = this;
 
-        for (var i = 0; i < max; i += 1) {
-            that.btns[i].addEventListener('click', Slider[that.btns[i].id].bind(null, that));
+
+
+
+
+
+
+class SliderCarousel {
+    constructor({
+        main,
+        wrap,
+        position = 0,
+        next,
+        prev,
+        slidesShow = 1,
+    }) {
+
+        this.main = document.querySelector(main);
+        this.wrap = document.querySelector(wrap);
+        this.slides = document.querySelector(wrap).children;
+        this.next = document.querySelector(next);
+        this.prev = document.querySelector(prev);
+        this.slidesShow = slidesShow;
+        this.options = {
+            position,
+            widthSlides: Math.floor(100 / this.slidesShow)
+        };
+
+
+    }
+
+
+    init() {
+        this.addMyClass();
+        this.addStyle();
+        if (this.prev && this.next) {
+            this.contrlolSlider();
+        } else {
+            this.addArrow();
+            this.contrlolSlider();
         }
-    }
 
-    Slider.prev = function(box) {
-        box.slideBox.style.transition = `transform .7s ease-in-out`;
-        var size = box.size;
-        index <= 0 ? false : index--
-            box.slideBox.style.transform = `translateX(${-index * size}px)`;
-        box.jump()
     };
 
-    Slider.next = function(box) {
-        box.slideBox.style.transition = `transform .7s ease-in-out`;
-        var max = box.slide.length;
-        var size = box.size;
-        index >= max - 1 ? false : index++;
-        box.slideBox.style.transform = `translateX(${-index * size}px)`;
-        box.jump();
+    addMyClass() {
+        this.main.classList.add('my-slider');
+        this.wrap.classList.add('my-slider__wrap');
+
+        for (const item of this.slides) {
+            item.classList.add('my-slider__item');
+        }
+    };
+
+    addStyle() {
+        const style = document.createElement('style');
+        style.id = 'slider-style';
+        style.textContent = `
+        .my-slider{
+            overflow: hidden !important;
+            
+        }
+        .my-slider__wrap{
+            display: flex !important;
+            transition: transform .5s !important;
+            will-change: transform !important;
+        }
+        .my-slider__item{
+            flex: 0 0  ${this.options.widthSlides }% !important;
+            padding: 0 30px
+            
+            
+        }
+        `;
+
+        document.head.appendChild(style);
+
     };
 
 
-    Slider.prototype.jump = function() {
-        var that = this;
-        var size = this.size;
-        this.slideBox.addEventListener('transitionend', function() {
-            that.slide[index].id === `firstClone` ? index = 1 : index;
-            that.slide[index].id === `lastClone` ? index = that.slide.length - 2 : index;
+    contrlolSlider() {
+        this.prev.addEventListener('click', this.prevSlider.bind(this));
+        this.next.addEventListener('click', this.nextSlider.bind(this));
+    };
 
-            that.slideBox.style.transition = `none`;
-            that.slideBox.style.transform = `translateX(${-index * size}px)`;
-        });
-
-
+    prevSlider() {
+        --this.options.position
+        console.log(this.options.position)
     }
 
-    new Slider();
+    nextSlider() {
+        ++this.options.position
+        console.log(this.options.position)
+    }
 
-})()
+    addArrow() {
+
+    };
+
+
+};
+
+const caruosel = new SliderCarousel({
+    main: '.slider-wrapper',
+    wrap: '.slider-slides',
+    next: '#next',
+    prev: '#prev',
+    slidesShow: 3
+});
+
+caruosel.init();
